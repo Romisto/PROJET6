6b0a9a4be88f3c13d77e8fec8b70a71b4d8141e9
Romain Aboa <fr.romisto@gmail.com>
2022-10-17 22:35:09 +0200

    creation des models

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 const http = require('http');
 const app = require('./app');
 const mongoose = require('mongoose');
+const model_sauces = require('./models/model_sauces');
+const model_users = require('./models/model_users');
+const sauces_controllers = require('./controllers/sauces');
 
 mongoose.connect('mongodb+srv://<arogi>:<Romain1102>@cluster0.yhctsrg.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+  app.get ('/sauces', sauces_controllers.getAllSauce);
 
 app.set('port', process.env.PORT || 3000);
 const server = http.createServer(app);
