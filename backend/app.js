8bff4f04e381f7459501cd0ff2a78e277bd7d0d6
Romain Aboa <fr.romisto@gmail.com>
2022-11-26 10:44:24 +0100

    modification variable environnement mongoose

--- backend/app.js
+++ backend/app.js
@@ -24,8 +24,7 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://" +
 process.env.MONGO_DB_USER +
 ":" +
-process.env.MONGO_DB_USER_MDP + "@cluster0.yhctsrg.mongodb.net/" +
-process.env.MONGO_DB_MARQUE,
+process.env.MONGO_DB_USER_MDP + "@cluster0.yhctsrg.mongodb.net/?retryWrites=true&w=majority",
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
